49873041ca4f14b1b804838c99958b387a02ce01
Dayan Ryspayev <izinor12@gmail.com>
2023-07-14 14:48:57 +0600

    Finalization of the game

--- Domain/Board.cs
+++ Domain/Board.cs
@@ -5,6 +5,9 @@
         public int ColumnCount => 3;
         public int RowCount => 3;
 
+        public bool Move_X = true;
+        public bool Move_O = false;
+
         public List<CellState> BoardCells = new List<CellState>()
         {
             CellState.Empty,
@@ -12,17 +15,32 @@
             CellState.Empty,
 
             CellState.Empty,
-            CellState.X,
+            CellState.Empty,
             CellState.Empty,
 
-            CellState.O,
+            CellState.Empty,
             CellState.Empty,
             CellState.Empty,
         };
 
+        public Dictionary<string, CellState> Cells = new()
+        {
+            { "Cell_x0_y0", CellState.Empty },
+            { "Cell_x1_y0", CellState.Empty },
+            { "Cell_x2_y0", CellState.Empty },
+
+            { "Cell_x0_y1", CellState.Empty },
+            { "Cell_x1_y1", CellState.Empty },
+            { "Cell_x2_y1", CellState.Empty },
+
+            { "Cell_x0_y2", CellState.Empty },
+            { "Cell_x1_y2", CellState.Empty },
+            { "Cell_x2_y2", CellState.Empty },
+        };
+
         // Row 1
-        public CellState Cell_x0_y0 { get; set; } = CellState.O;
-        public CellState Cell_x1_y0 { get; set; } = CellState.X;
+        public CellState Cell_x0_y0 { get; set; }
+        public CellState Cell_x1_y0 { get; set; }
         public CellState Cell_x2_y0 { get; set; }
         // Row 2
         public CellState Cell_x0_y1 { get; set; }
@@ -32,6 +50,29 @@
         public CellState Cell_x0_y2 { get; set; }
         public CellState Cell_x1_y2 { get; set; }
         public CellState Cell_x2_y2 { get; set; }
+
+
+
+        public void CellClick(int index)
+        {
+            if (BoardCells[index] == CellState.Empty && Move_X)
+            {
+                BoardCells[index] = CellState.X;
+                Move_X = false;
+                Move_O = true;
+            }
+            else if (BoardCells[index] == CellState.Empty && Move_O)
+            {
+                BoardCells[index] = CellState.O;
+                Move_X = true;
+                Move_O = false;
+            }
+        }
+
+        public void WinChecker()
+        {
+        
+        }
 
     }
 
